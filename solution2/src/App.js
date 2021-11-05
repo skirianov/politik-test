ecd30b53873dbb5ede66ae23c28b93a733517581
S.Kiri <s.kirianov@gmail.com>
2021-11-05 14:56:28 +0200

    solution2

---
+++ solution2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Button from './components/button/Button';
+
+const App = () => {
+
+    return (
+    <div>
+      <Button value="" />
+      <Button value="id" data="cantons" />
+      <Button value="name"  />
+    </div>
+  )
+}
+
+export default App;
---
+++ solution2/src/components/button/Button.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Button = ({ value, data }) => {
+
+  //for local development environment to prevent cors
+  // replace proxy with http://ws-old.parlament.ch/ for prod
+  const proxy = 'http://localhost:8010/proxy/'; 
+
+  //if data not provided fetch councillors by default
+  const getCouncillors = async (filter = null, data = 'councillors') => { 
+    if (filter) {
+      const filterLower = filter.toLowerCase();
+      switch (filterLower) {
+        case 'id':
+          await fetch(`${proxy}${data}?format=json`)
+            .then(response => response.json())
+            .then(data => {
+              let sorted = data.sort((a, b) => a.id - b.id);
+              return sorted;
+            });
+          break;
+        case 'name':
+          const nameType = data === 'councillors' ? 'lastName' : 'name';
+
+          await fetch(`${proxy}${data}?format=json`)
+            .then(response => response.json())
+            .then(data => {
+              let sorted = data.sort((a, b) => {
+                if (a[nameType] < b[nameType]) {
+                  return -1;
+                } else if (a[nameType] > b[nameType]) {
+                  return 1;
+                }
+                return 0;
+              });
+              return sorted;
+            });
+          break;
+        default:
+          return null;
+      }
+    } else {
+      await fetch(`${proxy}${data}?format=json`)
+        .then(response => response.json())
+        .then(data => { return data });
+    }
+  }
+
+  return (
+    <button 
+      value={value}
+      onClick={() => getCouncillors(value, data)}
+    >
+      Get {data || 'councillors'} {value ? `sorted by ${value}` : null }
+    </button>
+  )
+}
+
+export default Button;
